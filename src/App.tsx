3c783bd065b2a3620ab339d5d486b86a44674ef1
Khalil <khalilpati55@gmail.com>
2024-08-04 02:09:14 +0530

    React + Vite + Tailwind Boilerplate

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <main className="p-4 flex h-screen items-center justify-center">
+      <h1 className="text-3xl font-bold">Vite + React + Tailwind</h1>
+    </main>
+  );
+}
